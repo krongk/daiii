8239e9701da9eab01e986f8c907c30e1be8bb3e6
krongk <kenrome@gmail.com>
2013-03-28 23:00:24 +0800

    update migrate

--- db/migrate/20130326085239_add_devise_to_admin_users.rb
+++ db/migrate/20130326085239_add_devise_to_admin_users.rb
@@ -1,49 +1,65 @@
 class AddDeviseToAdminUsers < ActiveRecord::Migration
-  def self.up
-    change_table(:admin_users) do |t|
-      ## Database authenticatable
-      t.string :email,              :null => false, :default => ""
-      t.string :encrypted_password, :null => false, :default => ""
-
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      ## Rememberable
-      t.datetime :remember_created_at
-
-      ## Trackable
-      t.integer  :sign_in_count, :default => 0
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
-
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-      ## Token authenticatable
-      # t.string :authentication_token
-
-
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps
-    end
+  def change
+    add_column :admin_users, :email, :null  => false, :default => ""
+    add_column :admin_users, :encrypted_password, :null  => false, :default => ""
+    add_column :admin_users, :reset_password_token
+    add_column :admin_users, :reset_password_sent_at
+    add_column :admin_users, :remember_created_at
+    add_column :admin_users, :sign_in_count, :default => 0
+    add_column :admin_users, :current_sign_in_at
+    add_column :admin_users, :last_sign_in_at
+    add_column :admin_users, :current_sign_in_at
+    add_column :admin_users, :current_sign_in_ip
+    add_column :admin_users, :last_sign_in_ip
 
     add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
-    # add_index :admin_users, :confirmation_token,   :unique => true
-    # add_index :admin_users, :unlock_token,         :unique => true
-    # add_index :admin_users, :authentication_token, :unique => true
   end
+  # def self.up
+  #   change_table(:admin_users) do |t|
+  #     ## Database authenticatable
+  #     t.string :email,              :null => false, :default => ""
+  #     t.string :encrypted_password, :null => false, :default => ""
+
+  #     ## Recoverable
+  #     t.string   :reset_password_token
+  #     t.datetime :reset_password_sent_at
+
+  #     ## Rememberable
+  #     t.datetime :remember_created_at
+
+  #     ## Trackable
+  #     t.integer  :sign_in_count, :default => 0
+  #     t.datetime :current_sign_in_at
+  #     t.datetime :last_sign_in_at
+  #     t.string   :current_sign_in_ip
+  #     t.string   :last_sign_in_ip
+
+  #     ## Confirmable
+  #     # t.string   :confirmation_token
+  #     # t.datetime :confirmed_at
+  #     # t.datetime :confirmation_sent_at
+  #     # t.string   :unconfirmed_email # Only if using reconfirmable
+
+  #     ## Lockable
+  #     # t.integer  :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
+  #     # t.string   :unlock_token # Only if unlock strategy is :email or :both
+  #     # t.datetime :locked_at
+
+  #     ## Token authenticatable
+  #     # t.string :authentication_token
+
+
+  #     # Uncomment below if timestamps were not included in your original model.
+  #     # t.timestamps
+  #   end
+
+  #   add_index :admin_users, :email,                :unique => true
+  #   add_index :admin_users, :reset_password_token, :unique => true
+  #   # add_index :admin_users, :confirmation_token,   :unique => true
+  #   # add_index :admin_users, :unlock_token,         :unique => true
+  #   # add_index :admin_users, :authentication_token, :unique => true
+  # end
 
   def self.down
     # By default, we don't want to make any assumption about how to roll back a migration when your
