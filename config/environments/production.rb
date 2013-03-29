99d99571389b5eb484784e0314971ce4b1e61778
krongk <kenrome@gmail.com>
2013-03-30 01:33:58 +0800

    update last

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ RailsComposerApp2::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'cocacocan.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
