e58667a86b08155faafc87ab4d3bda73900166b8
krongk <kenrome@gmail.com>
2013-04-01 12:03:30 +0800

    update last all

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   end
 
   def modal_window
-  	sleep 2 #development only, simulates wait time
+     #	sleep 2 #development only, simulates wait time
 	  respond_to do |format|
 	    #format.html { redirect_to "home/modal_window" } #for my controller, i wanted it to be JS only
 	    format.js
