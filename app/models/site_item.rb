e835467fc906dacf1273bc7c628fb69a41352e9e
krongk <kenrome@gmail.com>
2013-05-27 21:48:52 +0800

    add all

--- app/models/site_item.rb
+++ app/models/site_item.rb
@@ -1,7 +1,7 @@
 class SiteItem < ActiveRecord::Base
   belongs_to :site_cate
   belongs_to :user
-  attr_accessible  :site_cate_id, :note, :rate_count, :site_password, :site_password_tips, :site_title, :site_url, :site_username, :visit_count
+  attr_accessible :site_cate_id, :note, :rate_count, :site_password, :site_password_tips, :site_title, :site_url, :site_username, :visit_count
   #validates :site_title, :presence => true
   validates :site_url, :presence => true
   #validates :site_url, :uniqueness => true
